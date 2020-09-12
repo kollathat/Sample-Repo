50f9aea4d1d9558e7be4b3901f917f2b39e496b9
Kollathat Petpatomchol <kollathat@gmail.com>
2020-09-13 02:10:01 +0700

    Integers & Floats - Working with Numeric Data

--- intro.py
+++ intro.py
@@ -1,11 +1,28 @@
-greeting = "Hello"
-name = "Kollathat"
+# Arithmetic Operators
+print(3 + 2)
+print(3 - 2)
+print(3 * 2)
+print(3 / 2)
+print(3 // 2)
+print(3 ** 2)
+print(3 % 2)
+print(abs(-3))
+print(round(3.75, 1))
 
-# message = greeting + ", " + name + ". Welcome!"
-# message = "{}, {} Welcome!".format(greeting, name)
-# message = f"{greeting}, {name.upper()}. Welcome!"
-# print(message)
+# Comparisons
+num_1 = 3
+num_2 = 2
 
-# print(dir(name))
+print(num_1 == num_2)
+print(num_1 != num_2)
+print(num_1 > num_2)
+print(num_1 < num_2)
+print(num_1 >= num_2)
+print(num_1 <= num_2)
 
-# print(help(str.lower))
+num_3 = "100"
+num_4 = "200"
+num_3 = int(num_3)
+num_4 = int(num_4)
+
+print(num_3 + num_4)
