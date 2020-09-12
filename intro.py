08cfbe6a05701a6015ad46b99f53c51d45402454
Kollathat Petpatomchol <kollathat@gmail.com>
2020-09-13 01:51:53 +0700

    Strings - Working with Textual Data

---
+++ intro.py
@@ -0,0 +1,11 @@
+greeting = "Hello"
+name = "Kollathat"
+
+# message = greeting + ", " + name + ". Welcome!"
+# message = "{}, {} Welcome!".format(greeting, name)
+# message = f"{greeting}, {name.upper()}. Welcome!"
+# print(message)
+
+# print(dir(name))
+
+# print(help(str.lower))
