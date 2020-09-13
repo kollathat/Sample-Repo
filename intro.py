a6e3c61f1e90b7687cbffd7d5fab2b91464bc0c5
Kollathat Petpatomchol <kollathat@gmail.com>
2020-09-13 22:16:33 +0700

    Lists, Tuples, and Sets

--- intro.py
+++ intro.py
@@ -1,28 +1,100 @@
-# Arithmetic Operators
-print(3 + 2)
-print(3 - 2)
-print(3 * 2)
-print(3 / 2)
-print(3 // 2)
-print(3 ** 2)
-print(3 % 2)
-print(abs(-3))
-print(round(3.75, 1))
+courses = ["History", "Math", "Physics", "CompSci"]
 
-# Comparisons
-num_1 = 3
-num_2 = 2
+"""
+# courses_2 = ["Arts", "Education"]
 
-print(num_1 == num_2)
-print(num_1 != num_2)
-print(num_1 > num_2)
-print(num_1 < num_2)
-print(num_1 >= num_2)
-print(num_1 <= num_2)
+# courses.append("Arts")
+# courses.insert(0, "Arts")
+# courses.insert(0, courses_2)
+# courses.extend(courses_2)
 
-num_3 = "100"
-num_4 = "200"
-num_3 = int(num_3)
-num_4 = int(num_4)
+# courses.remove("Math")
+# courses.pop()
+# poped = courses.pop()
 
-print(num_3 + num_4)
+# print(poped)
+
+nums = [1, 5, 2, 4, 3]
+
+# sort decending
+nums.sort(reverse=True)
+
+# sort ascending
+nums.sort()
+
+# courses.reverse()
+courses.sort(reverse=True)
+
+sorted_courses = sorted(courses)
+
+print(courses)
+print(sorted_courses)
+print(nums)
+print(sum(nums))
+print(courses.index("Math"))
+print("Arts" in courses)
+
+for item in courses:
+    print(item)
+
+for index, course in enumerate(courses, start=1):
+    print(index, course)
+
+course_str = " - ".join(courses)
+
+new_list = course_str.split(" - ")
+
+print(course_str)
+print(new_list)
+"""
+
+"""
+# mutable
+list_1 = ["History", "Math", "Physics", "CompSci"]
+list_2 = list_1
+
+print(list_1)
+print(list_2)
+
+list_1[0] = "Art"
+
+print(list_1)
+print(list_2)
+"""
+
+"""
+# immutable
+tuple_1 = ("History", "Math", "Physics", "CompSci")
+tuple_2 = tuple_1
+
+print(tuple_1)
+print(tuple_2)
+
+tuple_1[0] = "Art"
+
+print(tuple_1)
+print(tuple_2)
+"""
+
+"""
+# sets
+cs_courses = {"History", "Math", "Physics", "CompSci", "Math"}
+art_courses = {"History", "Math", "Art", "Design"}
+
+print(cs_courses)
+print("Math" in cs_courses)
+print(cs_courses.intersection(art_courses))
+print(cs_courses.difference(art_courses))
+"""
+
+# empty lists
+empty_list = []
+empty_list = list()
+
+# empty tuples
+empty_tuple = ()
+empty_tuple = tuple()
+
+# empty sets
+empty_set = {}  # this isn't right! it's a dictionary
+empty_set = set()
